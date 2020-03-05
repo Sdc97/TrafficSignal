3987d893950a231188fc446f2d9bffc162b4d8da
Steven Calvert <Sdc977@gmail.com>
2020-03-04 21:33:38 -0800

    First verilog added

---
+++ signal_vm.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:31:36 03/04/2020 
+// Design Name: 
+// Module Name:    signal_vm 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module signal_vm(
+	input wire s
+    );
+
+
+endmodule
